8b93dddee708b16c3531fd307c9d9d5bb72042d9
Fabian Schmitthenner <fabian1024@googlemail.com>
2010-11-27 12:51:25 +0100

    fix not showing data in halt mode

--- Fernbedienung/Menu.cpp
+++ Fernbedienung/Menu.cpp
@@ -354,7 +354,7 @@ void Menu::writeStromAndSpannung(long unsigned int spannung, long signed int str
 {
   using Fernbedienung::LcdHelper::lcd;
   using Fernbedienung::LcdHelper::writeCommaNumber;
-  if (mode == RUNNING)
+  if (mode == RUNNING && haltUntil < millis())
   {
     lcd.clear();
     lcd.setCursor (0, 0);
